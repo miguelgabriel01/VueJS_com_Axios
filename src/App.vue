a693eeaf11469084674b21cbcea3f4d2429491b2
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-31 15:32:06 -0300

    Criação do form de cadastro de usuarios, criação inicial do metodo que fará a requisição e implementação do botão que ativara a o metodo apos o click

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome">
+         <b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe seu Nome"></b-form-input>
+			</b-form-group>
+
+		<b-form-group label="E-mail">
+     <b-form-input type="email" size="lg" v-model="usuario.email" placeholder="Informe seu E-mail"></b-form-input>
+		</b-form-group>
+		<hr>
+		<b-button @click="salvar()" size="lg" variant="primary">Salvar</b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
+	data(){
+		return{
+			usuario:{
+			nome:'',
+			email:'',
+			}
+		}
+	},
+	methods:{
+		//Metodo responsavel por salvar o usuario e que será chamado apos o click
+		salvar(){
+			console.log(this.usuario)
+		}
+	}
  //metodo do ciclo de vida que é criado assim que a aplicação é iniciada
- created(){
+ //created(){
 	 /**
 		* usamos a variavel global $http que é criada no arquivo axios.js e aparti dele usamos o verbo "post" e fazemos uma requisição a url fornecida como padrão tambem no arquivo axios.js
 	  */
-	 this.$http.post('usuarios.json',{
+/* 	 this.$http.post('usuarios.json',{
 		 nome: "Miguel",
 		 email: "gabrielogabriel10@gmail.com"
 	 }).then(res => console.log(res))
- }
+ */ //}
 }
 </script>
 
